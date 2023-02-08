ea1ce1af567347a14ab2a3b2e3046fddf13b756c
richardmachage <rchacha261@gmail.com#>
2023-02-08 23:56:14 +0300

    check if user log in

--- app/src/main/java/com/example/social/Home/HomeActivity.java
+++ app/src/main/java/com/example/social/Home/HomeActivity.java
@@ -3,22 +3,54 @@ package com.example.social.Home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.social.LogIn.LogInActivity;
 import com.example.social.R;
+import com.example.social.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser() == null){ //if true it means no user is logged in
+                    Intent goToLogIn_intent = new Intent(HomeActivity.this, Log.class);
+                    goToLogIn_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //clears the current activity as well as all others on top of it in the stack so that user wont be able to go back
+                    startActivity(goToLogIn_intent);
+
+                }else{
+                    Toast.makeText(HomeActivity.this,"logged in as: "+ firebaseAuth.getCurrentUser().getEmail(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
     }
 
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
 }
